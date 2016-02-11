871124f4c603ccdd663dbc32cc47bc736b2f76d2
sigrunnsky <sigrunn.sky@colorado.edu>
2016-02-11 16:10:36 -0700

    BOMB.com

--- src/apps/uber/client/components/navbar.jsx
+++ src/apps/uber/client/components/navbar.jsx
@@ -3,12 +3,15 @@ class NavBar extends React.Component {
   render(){
     return (
       <nav>
-        <div className="nav-wrapper">
-        <a href="#" className="brand-logo">TODO</a>
+        <div className="nav-wrapper #ffc107 amber">
+        <span className= "black-text">
+        <a href="#" className="brand-logo">Chip Tracker</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="todo.html">TODO</a></li>
-          <li><a href="todo.html">TODO</a></li>          
+          <li><a href="../index.html">Home</a></li>
+          <li><a href="../client/index.html">Client</a></li>       
+          <li><a href="../admin/index.html">Admin</a></li>
         </ul>
+        </span>
         </div>
       </nav>
     );
