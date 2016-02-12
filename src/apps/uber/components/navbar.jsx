c138d9be0a51f10fa1b1e8a0976d17c2046d65db
CMcGuinness <comc3361@colorado.edu>
2016-02-11 17:18:22 -0700

    fixed nav bar links to correct address

--- src/apps/uber/components/navbar.jsx
+++ src/apps/uber/components/navbar.jsx
@@ -7,9 +7,9 @@ class NavBar extends React.Component {
         <span className= "black-text">
         <a href="#" className="brand-logo">Chip Tracker</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="../index.html">Home</a></li>
-          <li><a href="../client/index.html">Client</a></li>       
-          <li><a href="../admin/index.html">Admin</a></li>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="client/index.html">Client</a></li>       
+          <li><a href="admin/index.html">Admin</a></li>
         </ul>
         </span>
         </div>
