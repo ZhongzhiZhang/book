9ec2645643fd4f6aaa152e45997d9d0d3bd111b3
Zach Lamb <zala3440@colorado.edu>
2016-02-11 19:59:36 -0700

    reordering divs on index

--- src/apps/uber/admin/components/navbar.jsx
+++ src/apps/uber/admin/components/navbar.jsx
@@ -8,7 +8,7 @@ class NavBar extends React.Component {
         <a href="#" className="brand-logo">Chip Tracker</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="../index.html">Home</a></li>
-          <li><a href="../client/index.html">Client</a></li>       
+                    <li><a href="../client/index.html">Client</a></li>       
           <li><a href="../admin/index.html">Admin</a></li>
         </ul>
         </span>
