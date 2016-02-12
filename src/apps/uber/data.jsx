e282da5a2e133715832019b370fc084407afb66d
CMcGuinness <comc3361@colorado.edu>
2016-02-11 17:15:27 -0700

    added nav bar to home page

--- src/apps/uber/data.jsx
+++ src/apps/uber/data.jsx
@@ -24,7 +24,7 @@ function render(){
 // DATA
 //
 
-var firebaseRef = new Firebase('https://ucdd2-book.firebaseio.com/uber')
+var firebaseRef = new Firebase('https://platano.firebaseio.com/uber')
 
 // Real-time Data (load constantly on changes)
 firebaseRef.child('providers')
